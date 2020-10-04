24d673b9cf0dca44402dd2c15fd2a76f98c73ba4
ashraf abdellatif esmail <ashraf.fci2005@outlook.com>
2020-10-04 21:59:54 +0200

    initial one

---
+++ ZwajApp-SPA/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {  HttpClientModule } from "@angular/common/http";
+import { ValueComponent } from './Value/Value.component';
+
+@NgModule({
+  declarations: [	
+    AppComponent,
+      ValueComponent
+   ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ ZwajApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ZwajApp.API.Data;
+
+namespace ZwajApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        DataContext _datacontext;
+        public ValuesController(DataContext datacontext )
+        {
+            _datacontext = datacontext;
+        }
+        // GET api/values
+        [HttpGet]
+        public async Task<ActionResult> GetValues()
+        {
+            var Values = await  _datacontext.Values.ToListAsync();
+            return Ok(Values);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetValue(int id)
+        {
+            var Value = await  _datacontext.Values.FirstOrDefaultAsync(x=>x.id ==id);
+            return Ok(Value); 
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
